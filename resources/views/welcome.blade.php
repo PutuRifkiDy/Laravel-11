9b5964d9128d959dad52577736066f6ceb50c2e4
PutuRifkiDy <puturifki56@gmail.com>
2024-11-20 07:55:08 +0800

    set-up tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
+    @vite('resources/css/app.css')
 </head>
     <body>
-        <a href="/">welcome</a>
-        <a href="/about">about</a>
-        <a href="/blog">blog</a>
-        <a href="/contact">contact</a>
+        <div class="w-full flex flex-row justify-between px-24 text-[24px] font-bold">
+            <a href="/">welcome</a>
+            <a href="/about">about</a>
+            <a href="/blog">blog</a>
+            <a href="/contact">contact</a>
+        </div>
     </body>
 </html>
