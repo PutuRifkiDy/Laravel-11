361e1e0fe14bc9ce6303ae1abba5109af0923ace
PutuRifkiDy <puturifki56@gmail.com>
2025-03-02 11:56:53 +0800

    SET UP APLIKASI TO-DOLIST

--- routes/web.php
+++ routes/web.php
@@ -6,23 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about', [
-        "nama" => "Putu Rifki Dirkayuda"
-    ]);
-});
 
-Route::get('/blog', function () {
-    return view('blog', [
-        "contacts" => [
-            ["nama" => "Putu Rifki Dirkayuda", "pesan" => "Welcome to my blog"],
-            ["nama" => "Nyoman Tri Darma Wahyudi", "pesan" => "Welcome to my blog"]
-        ]
-    ]);
-});
-
-Route::get('/contact', function () {
-    return view('contact', [
-        "notelepon" => "0881038644484"
-    ]);
-});
